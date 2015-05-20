e891fc0989e561b6e915fdaff360df39eb331226
Matt Davis <matt.bronumski@btinternet.com>
2015-05-20 14:46:37 -0700

    Trying to get the build to work on appveyor

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 @echo off
 cls
 "tools\nuget\nuget.exe" "Install" "packages.config" "-OutputDirectory" "packages" "-ExcludeVersion"
+"tools\nuget\nuget.exe" "Restore" "src\HealthNet.sln"
 "packages\FAKE\tools\Fake.exe" build.fsx
