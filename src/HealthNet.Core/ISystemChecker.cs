9438ea45c973641c4f24236998c9a636dc673bc6
Matt Davis <matt.bronumski@btinternet.com>
2015-06-02 13:39:46 -0700

    Removed a test system checker that was left hanging around in a code base

--- src/HealthNet.Core/ISystemChecker.cs
+++ src/HealthNet.Core/ISystemChecker.cs
@@ -7,20 +7,4 @@
         string SystemName { get; }
         bool IsVital { get; }
     }
-
-    public class TestSystemChecker : ISystemChecker
-    {
-        public SystemCheckResult CheckSystem()
-        {
-            if (true == true)
-            {
-                return this.CreateCriticalResult("Some error");
-            }
-            return this.CreateGoodResult();
-        }
-
-        public bool IsIntrusive { get { return false; } }
-        public string SystemName { get { return "Test Checker"; } }
-        public bool IsVital { get { return true; } }
-    }
 }
