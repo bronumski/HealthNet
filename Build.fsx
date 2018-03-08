9287957b52339932b80559567472af1a9529645d
Matt Davis <bronumski@gmail.com>
2018-03-08 12:16:57 -0800

    #5 - Wired up fake to compile and pack using dotnet cli

--- Build.fsx
+++ Build.fsx
@@ -2,91 +2,78 @@
 
 open Fake
 open Fake.AssemblyInfoFile
-open Fake.AssemblyInfoHelper
 open Fake.Git
-
 let srcDir = "./src"
-let packagesDir = "./packages"
-let toolsDir = "./tools"
-let testDir = srcDir + "/tests/"
-let slnPath = srcDir + "/HealthNet.sln"
 
 let versionMajorMinor = "1.2"
 
 let commitHash = Information.getCurrentSHA1("")
 
 let versionMacroBuild = 
-    match buildServer with
-    | AppVeyor -> ("0." + appVeyorBuildVersion.Replace("1.1.", ""))
-    | _ -> "0"
+  match buildServer with
+  | AppVeyor -> ("0." + appVeyorBuildVersion.Replace("1.1.", ""))
+  | _ -> "0"
 
 let buildVersion = versionMajorMinor + "." + versionMacroBuild
 
-Target "Clean" (fun _ ->
-    CleanDirs []
-    DeleteDir "bin"
-    CreateDir "bin"
-)
-
 Target "Version" (fun _ ->
-    CreateCSharpAssemblyInfo (srcDir + "/VersionInfo.cs")
-        [Attribute.Version buildVersion
-         Attribute.FileVersion buildVersion
-         Attribute.Metadata("githash", commitHash)]
-
-    match buildServer with
-    | _ -> ()
+  CreateCSharpAssemblyInfo (srcDir @@ "VersionInfo.cs")
+    [Attribute.Metadata("githash", commitHash)]
 )
 
-Target "RestorePackages" (fun _ -> 
-     slnPath
-     |> RestoreMSSolutionPackages (fun p ->
-         { p with
-             OutputPath = packagesDir
-             ToolPath = (toolsDir + "/Nuget/nuget.exe")
-             Retries = 4 }))
+Target "Clean" (fun _ ->
+  CleanDirs []
+  DeleteDir "bin"
+  CreateDir "bin"
+)
 
 Target "Build" (fun _ ->
-    !! slnPath
-        |> MSBuildReleaseExt "" [("Configuration", "Release")] "Build"
-        |> Log "AppBuild-Output: "
+  DotNetCli.Build (fun p -> 
+    { p with
+        WorkingDir = srcDir
+        Configuration = "Release"
+        AdditionalArgs = ["/p:Version=" + versionMajorMinor + ".0"
+                          "/p:FileVersion=" + buildVersion
+                          "/p:InformationalVersion=" + versionMajorMinor
+                          "/p:Product=HealthNet"
+                          "/p:Company=HealthNet"
+                          "/p:Copyright=\"Copyright HealthNet " + System.DateTime.Now.Year.ToString() + "\""] })
 )
 
 Target "Test" (fun _ ->
-    let testResultFile = testDir + "TestResults.xml"
-    !! (testDir + "**/bin/Release/*Tests.dll")
-        |> NUnit (fun p ->
-            {p with
-                ToolPath = "./packages/NUnit.Runners/tools"
-                ToolName = "nunit-console-x86.exe"
-                DisableShadowCopy = true
-                OutputFile = testResultFile })
+  DotNetCli.Test (fun p ->
+    { p with
+        Configuration = "Release"
+        Project = "src\\Tests\\HealthNet.Tests"
+        AdditionalArgs = [ "--no-build" ] })
 )
 
 Target "CreatePackage" (fun _ ->
-    // Copy all the package files into a package folder
-    for nuspec in !! (srcDir + "/**/*.nuspec") do
-
-        let projFileName = nuspec.Replace(".nuspec", ".csproj")
-        
-        NuGetPack (fun p -> 
-            {p with
-                OutputPath = "bin"
-                WorkingDir = "bin"
-                Version = buildVersion
-                IncludeReferencedProjects = true
-                Properties = [ ("configuration", "release") ]
-                }) 
-                projFileName
+  // Copy all the package files into a package folder
+  for projFile in !! (srcDir @@ "/*/*.csproj") do
+    let projectName = (System.IO.FileInfo projFile).Directory.Name
+    DotNetCli.Pack(fun p ->
+    { p with
+        Configuration = "Release"
+        Project = projFile
+        OutputPath = "../../bin"
+        AdditionalArgs = [ "--no-build"
+                           "--no-restore"
+                           "/p:Authors=bronumski"
+                           "/p:PackageIconUrl=https://raw.githubusercontent.com/bronumski/HealthNet/release" + buildVersion + "/src/" + projectName + "/icon.png"
+                           "/p:PackageProjectUrl=https://github.com/bronumski/HealthNet"
+                           "/p:PackageLicenseUrl=https://raw.githubusercontent.com/bronumski/HealthNet/release/" + buildVersion + "/LICENSE"
+                           "/p:RepositoryUrl=git@github.com:bronumski/HealthNet.git"
+                           "/p:Description=HealthNet"
+                           "/p:Copyright=\"Copyright HealthNet " + System.DateTime.Now.Year.ToString() + "\""
+                           "/p:PackageVersion=" + buildVersion ] })
 )
 
 Target "Default" (fun _ ->
-    trace "Build Complete"
+  trace "Build Complete"
 )
 
 "Clean"
- ==> "Version"
- ==> "RestorePackages"
  ==> "Build"
  ==> "Test"
  ==> "CreatePackage"
--- src/ProductInfo.cs
+++ src/ProductInfo.cs
@@ -1,9 +1,5 @@
 using System;
 using System.Reflection;
 
-[ assembly: CLSCompliant( true ) ]
-[assembly: AssemblyProduct("HealthNet")]
-[assembly: AssemblyCompany("HealthNet")]
-[assembly: AssemblyCopyright("Copyright © HealthNet 2015")]
-[ assembly: AssemblyTrademark( "" ) ]
-[ assembly: AssemblyCulture( "" ) ]
+[assembly: CLSCompliant( true ) ]
+//[assembly: AssemblyTrademark( "HealthNet" ) ]
